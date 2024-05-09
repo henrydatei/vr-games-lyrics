a3370e972c98eab6a54fc7f50f858f1451b1ff90
henrydatei <31316025+henrydatei@users.noreply.github.com>
2024-05-09 21:36:39 +0200

    refactor: Add logging to Timer class for debugging purposes

--- classes/Timer.py
+++ classes/Timer.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 class Timer:
     def __init__(self, speed: float = 1):
@@ -7,18 +8,27 @@ class Timer:
         self.paused_duration = 0
         self.is_running = False
         self.speed = speed
+        
+        logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
 
     def start(self):
+        self.logger.debug("Timer starting")
         if not self.is_running:
+            self.logger.debug("Timer is not running, starting it")
             self.start_time = time.time()
             self.is_running = True
+            self.logger.debug("Timer started")
 
     def stop(self):
+        self.logger.debug("Timer stopping")
         if self.is_running:
+            self.logger.debug("Timer is running, stopping it")
             self.is_running = False
             self.start_time = None
             self.pause_time = None
             self.paused_duration = 0
+            self.logger.debug("Timer stopped")
 
     def pause(self):
         if self.is_running and self.pause_time is None:
